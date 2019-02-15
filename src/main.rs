c4b84ed3c51a0b14a627b95d216f698a4128db05
Michael <Michael@LAZEAR-WORK>
2019-02-15 14:30:35 -0800

    Basic REPL loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,65 @@
 mod syntax;
 
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::io::{self, prelude::*};
+
+fn display_usage() {
+    let usage = "zymogen";
+}
+
+fn main() -> io::Result<()> {
+    let mut inputs = Vec::new();
+    for arg in env::args().skip(1) {
+        match arg.as_ref() {
+            "--help" | "-h" => display_usage(),
+            _ => match fs::read_to_string(&arg) {
+                Ok(data) => inputs.push(data),
+                Err(e) => panic!("Error encountered while trying to read {}: {}", arg, e),
+            },
+        }
+    }
+    println!("zymogen interpreter");
+    for s in inputs {
+        println!("{}", s);
+    }
+
+    println!("REPL mode:");
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    loop {
+        let mut indent: u32 = 0;
+        let mut depth: i32 = 0;
+        loop {
+            handle.read_line(&mut buffer)?;
+            let left = buffer.matches('(').count() as i32;
+            let right = buffer.matches(')').count() as i32;
+            if left == right {
+                break;
+            } 
+
+            // Do some pretty formatting for indent levels
+            let delta = left - right;
+            if delta > depth {
+                indent += 4;
+                depth = delta;
+            } else if delta < depth {
+                indent -= 4;
+                depth = delta;
+            }
+            print!("{}", (0..indent).map(|_| ' ').collect::<String>());
+            io::stdout().flush()?;
+        }
+        let tokens = match syntax::lex(&buffer) {
+            Ok(tokens) => tokens,
+            Err(e) => {
+                eprint!("{}", e);
+                break;
+            }
+        };
+        println!("===> {:#?}", tokens);
+        buffer.clear();
+    }
+    Ok(())
 }
--- src/syntax/mod.rs
+++ src/syntax/mod.rs
@@ -1,3 +1,41 @@
 mod error;
 mod lexer;
 mod token;
+pub use error::{Error, ErrorKind};
+pub use lexer::Lexer;
+pub use token::{Token, TokenKind};
+
+pub fn lex<S: AsRef<str>>(s: S) -> Result<Vec<Token>, String> {
+    match Lexer::new(s.as_ref()).lex() {
+        Ok(v) => Ok(v),
+        Err(e) => {
+            let msg = match e.kind {
+                ErrorKind::EOF => "unexpected end-of-file",
+                ErrorKind::Invalid(ch) => "invalid character in input",
+            };
+
+            if e.line > 2 {
+                let t = e.line as usize;
+                let lines = s
+                    .as_ref()
+                    .lines()
+                    .skip(e.line as usize - 2)
+                    .take(2)
+                    .map(String::from)
+                    .collect::<Vec<String>>();
+                Err(format!(
+                    "...\n{}\n{}^~~{}\n",
+                    lines.join("\n"),
+                    (0..e.pos).map(|_| ' ').collect::<String>(),
+                    msg
+                ))
+            } else {
+                Err(format!(
+                    "{}{}^~~\n",
+                    s.as_ref(),
+                    (0..e.pos).map(|_| ' ').collect::<String>()
+                ))
+            }
+        }
+    }
+}
