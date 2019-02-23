e0573a4cf9cd23bff2ac6d8a2b031809651b7fd0
Michael Lazear <lazear@scripps.edu>
2019-02-22 23:24:35 -0800

    Pretty printing of MIR

--- src/compiler/ir/mir.rs
+++ src/compiler/ir/mir.rs
@@ -25,39 +25,22 @@ pub enum Expr {
     Quote(Value),
 }
 
-fn display_nested_let(e: &Expr, lvl: u32) -> String {
+fn display(e: &Expr, lvl: u32) -> String {
     let mut indent = (0..lvl * 4).map(|_| ' ').collect::<String>();
     let out = match e {
-        Expr::Let(var, val, body) => format!(
-            "(let (({} {}))\n{})",
-            var,
-            val,
-            display_nested_let(body, lvl + 1)
-        ),
-        _ => format!("{}", e),
-    };
-    indent.push_str(&out);
-    indent
-}
-
-impl fmt::Display for Expr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Expr::Val(v) => write!(f, "{}", v),
-            Expr::Var(s) => write!(f, "{}", s),
-            Expr::Let(var, val, body) => write!(
-                f,
+            Expr::Val(v) => format!("{}", v),
+            Expr::Var(s) => format!("{}", s),
+            Expr::Let(var, val, body) => format!(
                 "(let (({} {}))\n{})",
                 var,
                 val,
-                display_nested_let(body, 1)
+                display(body, lvl+1)
             ),
-            Expr::Lambda(var, None, body) => write!(f, "(λ ({}) {})", var.join(" "), body),
+            Expr::Lambda(var, None, body) => format!("(λ ({})\n{})", var.join(" "), display(body, lvl+1)),
             Expr::Lambda(var, Some(rest), body) => {
-                write!(f, "(λ ({} . {}) {})", var.join(" "), rest, body)
+                format!("(λ ({} . {})\n{})", var.join(" "), rest, display(body, lvl+1))
             }
-            Expr::App(rator, rand) => write!(
-                f,
+            Expr::App(rator, rand) => format!(
                 "({} {})",
                 rator,
                 rand.iter()
@@ -65,11 +48,47 @@ impl fmt::Display for Expr {
                     .collect::<Vec<String>>()
                     .join(" ")
             ),
-            Expr::If(test, csq, None) => write!(f, "(if {} {})", test, csq),
-            Expr::If(test, csq, Some(alt)) => write!(f, "(if {} {} {})", test, csq, alt),
-            Expr::Set(var, exp) => write!(f, "(set! {} {})", var, exp),
-            Expr::Quote(Value::Nil) => write!(f, "'()"),
-            Expr::Quote(exp) => write!(f, "'{}", exp),
-        }
+            Expr::If(test, csq, None) => format!("(if {}\n{})", test, display(csq, lvl+1)),
+            Expr::If(test, csq, Some(alt)) => format!("(if {}\n{}\n{})", test, display(csq, lvl + 1), display(alt, lvl + 1)),
+            Expr::Set(var, exp) => format!("(set! {}\n{})", var, display(exp, lvl+1)),
+            Expr::Quote(Value::Nil) => format!("'()"),
+            Expr::Quote(exp) => format!("'{}", exp),
+        };
+    indent.push_str(&out);
+    indent
+}
+
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", display(self, 0))
+        // match self {
+        //     Expr::Val(v) => write!(f, "{}", v),
+        //     Expr::Var(s) => write!(f, "{}", s),
+        //     Expr::Let(var, val, body) => write!(
+        //         f,
+        //         "(let (({} {}))\n{})",
+        //         var,
+        //         val,
+        //         display_nested_let(body, 1)
+        //     ),
+        //     Expr::Lambda(var, None, body) => write!(f, "(λ ({}) {})", var.join(" "), body),
+        //     Expr::Lambda(var, Some(rest), body) => {
+        //         write!(f, "(λ ({} . {}) {})", var.join(" "), rest, body)
+        //     }
+        //     Expr::App(rator, rand) => write!(
+        //         f,
+        //         "({} {})",
+        //         rator,
+        //         rand.iter()
+        //             .map(|e| format!("{}", e))
+        //             .collect::<Vec<String>>()
+        //             .join(" ")
+        //     ),
+        //     Expr::If(test, csq, None) => write!(f, "{}", test, csq),
+        //     Expr::If(test, csq, Some(alt)) => write!(f, "(if {} {} {})", test, csq, alt),
+        //     Expr::Set(var, exp) => write!(f, "(set! {} {})", var, exp),
+        //     Expr::Quote(Value::Nil) => write!(f, "'()"),
+        //     Expr::Quote(exp) => write!(f, "'{}", exp),
+        // }
     }
 }
